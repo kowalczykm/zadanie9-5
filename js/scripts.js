d927668f0f4ccd522de64600d38e3d6d310d6b19
marek <mkrzkowalczyk@gmail.com>
2017-07-06 18:55:00 +0200

    items

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-var buttons = document.getElementsByClassName('button');
-var arrayLength = buttons.length;
-for (var i = 0;i <= arrayLength; i++) {
-    alert(buttons[i].innerText);
 
-}
+var add = document.getElementById('addElem');
+var list = document.getElementById('list');
+
+var itemsByTagName = document.getElementsByTagName('li');
+
+
+add.addEventListener('click', function(e) {
+ 	var newElem = document.createElement('li');
+
+	newElem.innerHTML = 'item ' + itemsByTagName.length;
+	list.appendChild(newElem);
+	console.log(itemsByTagName.length - 1);
+});
+
